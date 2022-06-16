1b8e85e3eb303822a392e930866a767f1ddbc554
yams-yams <uma@kothuri.net>
2022-06-16 21:13:32 +0100

    Handles user input recursively instead of in a while loop, indentation fixed

--- copymain.ml
+++ copymain.ml
@@ -1,4 +1,3 @@
-(*Change tab size to 2*)
 type action = ADD | REMOVE | MODIFY | RENAMED_OLD | RENAMED_NEW
 type t
 
@@ -18,31 +17,27 @@ let handle_notif action filename =
     | RENAMED_OLD -> Printf.printf "Renamed from: %s\n%!" filename
     | RENAMED_NEW -> Printf.printf "          to: %s\n%!" filename
 
-
 let block state =
-    block_for_changes state handle_notif
-
+  block_for_changes state handle_notif
 
 let initial_paths handle = 
-    match Array.to_list Sys.argv with
-        | [] -> Printf.printf "No directories given\n%!"
-        | h::t -> List.iter (add_path handle) t
+  match Array.to_list Sys.argv with
+    | [] -> Printf.printf "No directories given\n%!"
+    | h::t -> List.iter (add_path handle) t
+
+let rec watch_input state handle =
+  match input_line stdin with
+    | "exit" ->
+      stop_watching state ;
+      Thread.join handle;
+      Printf.printf "File-watching has ended, main thread will exit now\n%!"
+    | _ as path -> 
+      add_path state path;
+      watch_input state handle
 
 let () =
-    let state = open_port () in
-    initial_paths state;
-    let ocaml_handle = Thread.create block state in
-    Printf.printf "Type another path to watch or 'exit' to end directory watching\n%!";
-    let flag = ref true in
-
-    (*change to recursion*)
-    while !flag do
-        match input_line stdin with
-        | "exit" ->
-            stop_watching state;
-            Thread.join ocaml_handle;
-            Printf.printf "File-watching has ended, main thread will exit now\n%!";
-            flag := false
-        | _ as path -> add_path state path
-    done
-
+  let state = open_port () in
+  initial_paths state;
+  let handle = Thread.create block state in
+  Printf.printf "Type another path to watch or 'exit' to end directory watching\n%!";
+  watch_input state handle;
